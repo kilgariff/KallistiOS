17501568f1d3571074261e79fadd027e22a9c808
Ross Kilgariff <rosskilgariff@gmail.com>
2025-08-08 15:40:15 +0100

    WIP: Fix arch_exec

--- kernel/arch/dreamcast/kernel/exec.c
+++ kernel/arch/dreamcast/kernel/exec.c
@@ -31,21 +31,33 @@ void arch_exec_at(const void *image, uint32 length, uint32 address) {
     uint32  tstart = (uint32)_arch_exec_template,
             tend = (uint32)_arch_exec_template_end;
     uint32  tcount = (tend - tstart) / 4;
-    uint32  buffer[tcount];
+
+    /* Instead of placing the trampoline on the stack where it might actually get overwritten by the new binary
+       while we're copying it in, put it at the very top of system RAM (assumes 16MiB of RAM present). */
+    // uint32  buffer[tcount];
+    uint32 * buffer = (uint32 *) (0xd000000 - (tend - tstart));
     uint32  * values;
     uint32 i;
 
-    assert((tend - tstart) % 4 == 0);
+    //assert((tend - tstart) % 4 == 0);
+
+    // dbglog(DBG_KDEBUG, "About to disable interrupts\n");
 
     /* Turn off interrupts */
     irq_disable();
 
+    // dbglog(DBG_KDEBUG, "About to flush the write cache for the specified image file\n");
+
     /* Flush the data cache for the source area */
     dcache_flush_range((uint32)image, length);
+
+    // dbglog(DBG_KDEBUG, "About to copy trampoline into end of main memory\n");
 
     /* Copy over the trampoline */
     for(i = 0; i < tcount; i++)
         buffer[i] = _arch_exec_template[i];
+
+    // dbglog(DBG_KDEBUG, "Copying over arguments\n");
 
     /* Plug in values */
     values = buffer + (_arch_exec_template_values - _arch_exec_template);
@@ -53,6 +65,8 @@ void arch_exec_at(const void *image, uint32 length, uint32 address) {
     values[1] = address;        /* Destination */
     values[2] = length / 4;     /* Length in uint32's */
     values[3] = _arch_old_stack;    /* Patch in old R15 */
+
+    // dbglog(DBG_KDEBUG, "Flushing caches\n");
 
     /* Flush both caches for the trampoline area */
     dcache_flush_range((uint32)buffer, tcount * 4);
@@ -63,8 +77,12 @@ void arch_exec_at(const void *image, uint32 length, uint32 address) {
         printf("%08x: %08x\n", (uint32)(buffer + i), buffer[i]);
     } */
 
+    // dbglog(DBG_KDEBUG, "arch_shutdown()\n");
+
     /* Shut us down */
     arch_shutdown();
+
+    // dbglog(DBG_KDEBUG, "still here?\n");
 
     /* Reset our old SR, VBR, and FPSCR */
     __asm__ __volatile__("ldc	%0,sr\n"
